2caf28db89883ae242dbc5600064cc048d38b6bc
minjyun3011 <grand3canyon10@gmail.com>
2023-11-21 20:10:59 +0900

    routeヘルパーまで

--- app/Http/Controllers/RememoController.php
+++ app/Http/Controllers/RememoController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rememo;
 use Illuminate\Http\Request;
 
 class RememoController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $memo = Rememo::find($id);
+        return view('rememos.show', ['memo' => $memo]);
+    }
+    public function index()
+    {
+        $memos = Rememo::all();
+        return view('rememos.index', ['memos' => $memos]);
+    }
 }
